0296ec94092225b657bdf556fdaecea8f9818cad
Anto-cozza <xsecondaria@gmail.com>
2025-02-21 15:29:58 +0100

    Create Monty_hall.py

---
+++ Monty_hall.py
@@ -0,0 +1,78 @@
+import streamlit as st
+from random import randint
+import matplotlib.pyplot as plt
+import numpy as np
+from time import sleep
+from random import shuffle, choice
+
+"""
+# Monty Hall Problem
+
+Hi dear player, we have three doors! 
+Behind one of them there is a car, behind the other a goat
+"""
+
+if not "exps" in st.session_state:
+    st.session_state.exps = []
+    
+if not "doors" in st.session_state:
+    st.session_state.doors = [True, False, False]
+    shuffle(st.session_state.doors)
+
+if not "monty_choose" in st.session_state:
+    st.session_state.monty_choose = False 
+
+if not "opened_door" in st.session_state:
+    st.session_state.opened_door = None
+    
+st.write("doors:", st.session_state.doors)
+
+chosen_door = st.number_input("Door", min_value = 0, max_value = 2, step = 1)
+
+st.write("You chose door number", chosen_door)
+
+prize_door = st.session_state.doors.index(True)
+
+doors_available_to_reveal = set([0,1,2]) - set([chosen_door]) - set([prize_door])
+    
+st.write(doors_available_to_reveal)
+
+if(not st.session_state.monty_choose):
+    if(st.button("Monty, please open a door!")):
+        st.session_state.opened_door = choice(list(doors_available_to_reveal))
+        st.session_state.monty_choose = True
+        st.rerun()
+else: 
+        st.write("Monty opened", st.session_state.opened_door)
+
+if(st.button("open my door")):
+    if st.session_state.doors[chosen_door] == True:
+        st.balloons()
+        st.session_state.exps.append(True)
+    else:
+        st.write("You loose!")
+        st.session_state.exps.append(False)
+
+if(st.button("New game")):
+    shuffle(st.session_state.doors)
+    st.session_state.monty_choose = False
+    st.session_state.opened_door = None
+    st.rerun()
+
+st.write(st.session_state.exps)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
